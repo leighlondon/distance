af929b1caaa8883484ffcfd6f973d3699274e958
Leigh London <leigh.00.london@gmail.com>
2015-12-03 20:26:07 +1100

    Document the exported function.

--- distance.go
+++ distance.go
@@ -1,6 +1,8 @@
 // Package distance calculates the edit distance between strings.
 package distance
 
+// Distance calculates the edit distance recursively between two
+// strings using the Levenshtein distance.
 func Distance(s1, s2 string) int {
 	// Check whether the strings are equal.
 	if s1 == s2 {
